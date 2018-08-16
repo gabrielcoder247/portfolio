0dbc37712ef706ab1a7bd74d8f1c443f246da973
wanjiiru <liz240@live.com>
2018-08-16 22:27:21 +0300

    add contact section

--- pfolio/models.py
+++ pfolio/models.py
@@ -6,6 +6,8 @@ class Project(models.Model):
     title = models.CharField(max_length = 60)
     description = models.TextField()
     image = models.ImageField(upload_to = 'uploads')
+    git_url = models.CharField(max_length = 66)
+    live_site = models.CharField(max_length=66)
 
     def __str__(self):
         return self.title
